84763f135fa0ff93254ea18086bf7ca6db3bd959
eliland <gJbiMBGt3i>
2017-02-16 15:59:59 +0100

    Ryddet markup + la til wordpress-importer

--- wp-content/plugins/ndla-api-gateway/includes/admin-menu.php
+++ wp-content/plugins/ndla-api-gateway/includes/admin-menu.php
@@ -25,34 +25,19 @@ function ndla_settings_page()
     // Read in existing option value from database
     $opt_val = get_option($opt_name);
 
-    // See if the user has posted us some information
-    // If they did, this hidden field will be set to 'Y'
     if (isset($_POST[$hidden_field_name]) && $_POST[$hidden_field_name] == 'Y') {
-        // Read their posted value
         $opt_val = $_POST[$data_field_name];
-
-        // Save the posted value in the database
         update_option($opt_name, $opt_val);
 
-        // Put a "settings saved" message on the screen
         ?>
         <div class="updated"><p><strong><?php _e('Settings saved.', 'ndla'); ?></strong></p></div>
         <?php
-
     }
-
-    // Now display the settings editing screen
-
-    echo '<div class="wrap">';
-
-    // header
-
-    echo "<h2>" . __('NDLA Plugin Settings', 'ndla') . "</h2>";
-
-    // settings form
 
     ?>
 
+    <div class="wrap">
+    <h2><?php _e('NDLA Plugin Settings', 'ndla');?></h2>
     <form name="form1" method="post" action="">
         <input type="hidden" name="<?php echo $hidden_field_name; ?>" value="Y">
 
@@ -65,7 +50,6 @@ function ndla_settings_page()
         <p class="submit">
             <input type="submit" name="Submit" class="button-primary" value="<?php esc_attr_e('Save Changes') ?>"/>
         </p>
-
     </form>
     </div>
 
