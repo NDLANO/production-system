ef602b899d09f9b996ff021bd73cfabbcfd1362f
Mathias Mølster Lidal <mathiaslidal@gmail.com>
2017-02-27 09:10:38 +0100

    fjern debug echo-linjer

--- deploy.php
+++ deploy.php
@@ -16,15 +16,13 @@ $description_placeholder = "<description>";
 
 $push_cmd = "aws deploy push --application-name NDLA_Produksjonssystem --s3-location s3://ndla-produksjonssystem-codedeploy/codedeploy.zip --source wp-content";
 
+echo $push_cmd;
+
 $deploy_cmd = shell_exec($push_cmd);
-echo "$deploy_cmd\n";
 $deploy_cmd = substr($deploy_cmd, strpos($deploy_cmd, "aws"));
-echo "$deploy_cmd\n";
 
 $deploy_cmd = str_replace($deployment_group_placeholder, $deployment_group, $deploy_cmd);
-echo "$deploy_cmd\n";
 $deploy_cmd = str_replace($deployment_config_placeholder, $deployment_config, $deploy_cmd);
-echo "$deploy_cmd\n";
 $deploy_cmd = str_replace($description_placeholder, $description, $deploy_cmd);
 
 
